375a79db9e6eece1b413a1d4533b0182d6c53b07
Robert Christensen <robert.christensen@mymail.champlain.edu>
2023-05-08 00:54:11 -0400

    Fixed clang freaking out for unusual file extensions

--- SanableEngine/build-tools/parse-ast.py
+++ SanableEngine/build-tools/parse-ast.py
@@ -1,7 +1,7 @@
 import clang.cindex
 import sys, os
 
-def parseAuto(target: str):
+def parseAuto(target: str) -> list[clang.cindex.Cursor]:
 	if os.path.isdir(target):
 		return parseDir(target)
 	elif os.path.splitext(target)[1] in [".h", ".hpp", ".inl", ".c", ".cpp"]:
@@ -9,17 +9,17 @@ def parseAuto(target: str):
 	else:
 		return []
 
-def parseDir(target: str):
+def parseDir(target: str) -> list[clang.cindex.Cursor]:
 	out = []
 	for subpath in os.listdir(target):
 		out += parseAuto(os.path.join(target, subpath))
 	return out
 
-def parseFile(target: str):
+def parseFile(target: str) -> list[clang.cindex.Cursor]:
 	index = clang.cindex.Index.create()
 
 	try:
-		tu = index.parse(target, args=["-std=c++17"], options=clang.cindex.TranslationUnit.PARSE_SKIP_FUNCTION_BODIES)
+		tu = index.parse(target, args=["-std=c++17", "--language=c++-header"], options=clang.cindex.TranslationUnit.PARSE_SKIP_FUNCTION_BODIES)
 		return [i for i in tu.cursor.get_children() if isOurs(target, i)]
 	except Exception as e:
 		print(f"Error parsing translation unit for target {target}")
@@ -31,9 +31,18 @@ def isOurs(sourceDir: str, i: clang.cindex.Cursor):
 def isClass(cursor: clang.cindex.Cursor) -> bool:
 	return i.kind in [clang.cindex.CursorKind.CLASS_DECL, clang.cindex.CursorKind.CLASS_TEMPLATE, clang.cindex.CursorKind.CLASS_TEMPLATE_PARTIAL_SPECIALIZATION]
 
-#test = parseAuto(r"engine/memory/public/RawMemoryPool.hpp")
-test = parseAuto(r"UserPlugin/")
-for i in test:
-	print(f"{i.displayname} : {i.kind}")
 
-print("")
+
+
+
+if __name__ == "__main__":
+	def _testRig(filePath):
+		print(f"Parsing {filePath}:")
+		for i in parseAuto(filePath):
+			print(f" - {i.displayname} : {i.kind}")
+		print("")
+
+	_testRig(r"UserPlugin/")
+	_testRig(r"PrimitivesPlugin/")
+	_testRig(r"engine/memory/public/RawMemoryPool.hpp")
+	_testRig(r"engine/")
