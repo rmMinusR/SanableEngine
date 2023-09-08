12baa4e8ae82a48a1c2da80e9d0f093876dc69d9
Robert Christensen <robert@rmMinusR.com>
2023-09-07 22:53:52 -0500

    Suppressed clang's invalid offsetof warnings for generated RTTI

--- SanableEngine/engine/core/template/rtti.template.cpp
+++ SanableEngine/engine/core/template/rtti.template.cpp
@@ -5,7 +5,13 @@
 //Dependency includes
 INCLUDE_DEPENDENCIES
 
+//Clang will complain about inappropriate use of offsetof. Tests suggest it's wrong.
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Winvalid-offsetof"
+
 void engine_reportTypes(ModuleTypeRegistry* registry)
 {
 GENERATED_RTTI
 }
+
+#pragma clang diagnostic pop
--- SanableEngine/engine/reflection/tools/rtti.template.cpp
+++ SanableEngine/engine/reflection/tools/rtti.template.cpp
@@ -5,7 +5,13 @@
 //Dependency includes
 INCLUDE_DEPENDENCIES
 
+//Clang will complain about inappropriate use of offsetof. Tests suggest it's wrong.
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Winvalid-offsetof"
+
 PLUGIN_C_API(void) plugin_reportTypes(ModuleTypeRegistry* registry)
 {
 GENERATED_RTTI
 }
+
+#pragma clang diagnostic pop
