74b9dd6e781c0157dc0f1193d28de88e529abf2a
Robert Christensen <robert@rmMinusR.com>
2023-09-25 22:29:55 -0500

    Switched move tester data types

--- SanableEngine/engine/memory/test/include/MoveTester.hpp
+++ SanableEngine/engine/memory/test/include/MoveTester.hpp
@@ -1,8 +1,9 @@
 #pragma once
 
+//This will have alignment 4 and size 12. Useful not just for checking that moves worked, but for detecting alignment shearing as well...
 struct MoveTester
 {
 	int a = 1;
-	char b = 2;
-	short c = 3;
+	int b = 2;
+	int c = 3;
 };
