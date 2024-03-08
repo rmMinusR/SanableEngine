108fe2b0ec89eb59ab8de7878364dedb0d30b176
Robert Christensen <robert@rmMinusR.com>
2024-03-08 16:37:17 -0500

    Initial LabelWidget impl

---
+++ SanableEngine/engine/core/src/gui/LabelWidget.cpp
@@ -0,0 +1,35 @@
+#include "gui/LabelWidget.hpp"
+
+#include "Renderer.hpp"
+
+LabelWidget::LabelWidget(Material* material, Font* font) :
+	material(material),
+	font(font)
+{
+}
+
+LabelWidget::~LabelWidget()
+{
+}
+
+void LabelWidget::setText(const std::wstring& newText)
+{
+}
+
+const Material* LabelWidget::getMaterial() const
+{
+	return material;
+}
+
+void LabelWidget::renderImmediate(Renderer* renderer)
+{
+	Rect<float> r = transform.getRect();
+	auto depth = transform.getRenderDepth();
+
+	renderer->errorCheck();
+
+	constexpr SDL_Color col{ 1, 1, 1, 1 };
+	renderer->drawText(*font, col, text, Vector3f(r.x, r.y, depth));
+	
+	renderer->errorCheck();
+}
